9208a51f5b715a915eb3522ec61d24bb6fad3a36
Simon Kärrman <simon.karrman@cygni.se>
2021-09-29 08:20:37 +0200

    Add gitignore

---
+++ TodoApi/Controllers/BasicAuthenticationAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    public class BasicAuthenticationAttribute: Attribute, IAuthorizationFilter
+    {
+        public const string AuthScheme = "Basic";
+        private const string AuthHeaderKey = "Authorization";
+        private const string EncodingId = "iso-8859-1";
+        private const char CredentialsSeparator = ':';
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+
+            var authHeaderValue = context.HttpContext
+                .Request.Headers[AuthHeaderKey];
+
+            if(AuthenticationHeaderValue.TryParse(authHeaderValue,
+                out var authHeader) && authHeader.Scheme == AuthScheme)
+            {
+                Encoding encoding = Encoding.GetEncoding(EncodingId);
+                var usrPwd = encoding.GetString(
+                    Convert.FromBase64String(authHeader.Parameter));
+                var credentials = usrPwd.Split(CredentialsSeparator);
+                if (credentials.Length == 2 && credentials[0] == "usr" &&
+                    credentials[1] == "pwd")
+                {
+                    return;
+                }
+            }
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
---
+++ TodoApi/Middleware/HttpLogger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+
+namespace TodoApi.Middleware
+{
+    public class HttpLogger
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpLogger(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = await FormatRequest(context.Request);
+
+            Console.WriteLine("- Request:\n" + request);
+
+            var originalBodyStream = context.Response.Body;
+
+            using var responseBody = new MemoryStream();
+
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            var response = await FormatResponse(context.Response);
+
+            Console.WriteLine("- Response:\n" + response + "\n");
+
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+
+        private static async Task<string> FormatRequest(HttpRequest request)
+        {
+            request.EnableBuffering();
+
+            var body = await ReadBody(request);
+
+            var bodyString = string.IsNullOrEmpty(body) ? "" : $"Body:\t{body}";
+
+            return $"{request.Method} " +
+                $"{request.Scheme}" +
+                $"{request.Host}{request.Path}{request.QueryString}\n" +
+                $"{GetHeaders(request)}"+
+                $"{bodyString}";
+        }
+
+        private static async Task<string> FormatResponse(HttpResponse response)
+        {
+            var body = JsonConvert.DeserializeObject(await ReadBody(response)).ToString();
+
+            var bodyString = string.IsNullOrEmpty(body) ? "" : $"\nBody:\n{body}";
+
+            var code = response.StatusCode;
+
+            return $"Status Code: {code} " +
+                $"({ReasonPhrases.GetReasonPhrase(code)})\n" +
+                $"{GetHeaders(response)}" +
+                $"{bodyString}";
+        }
+
+        private static async Task<string> ReadBody(dynamic message)
+        {
+            message.Body.Seek(0, SeekOrigin.Begin);
+
+            string body = await new StreamReader(message.Body).ReadToEndAsync();
+
+            message.Body.Seek(0, SeekOrigin.Begin);
+
+            return body;
+        }
+
+        private static string GetHeaders(dynamic request)
+        {
+            var headers = "";
+            foreach (var header in request.Headers)
+                headers += $"\t{header.Key}: {header.Value}\n";
+            return headers;
+        }
+    }
+}
